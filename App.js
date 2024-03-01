804096264835a529c9f847bb104bca5ae16d51d6
Yulchikks <yuliapelia19@gmail.com>
2024-03-01 12:09:19 +0200

    add styles

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartGameScreen from "./screens/StartGameScreen";
+// import background from './assets/images/'
 
-import StartGameScreen from './screens/StartGameScreen';
+
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground sourse={require('./assets/images/background.png')} resizeMode='cover' style={styles.rootScreen} >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+
+  },
+});
